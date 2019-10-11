21134d158d17929625c76302b72d0f8820651593
びくんびくん <big.stone.net@gmail.com>
2019-10-11 12:58:44 +0900

    add schema.rb

--- db/migrate/20191009041049_create_projects.rb
+++ db/migrate/20191009041049_create_projects.rb
@@ -4,7 +4,7 @@ class CreateProjects < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :identifier
       t.text :description
-      t.bool :is_public
+      t.boolean :is_public
 
       t.timestamps
     end
